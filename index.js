d4ff04e5895963bdf61f06b031360d2b0929f0f0
Etienne <etienne@corsaircms.com>
2017-06-01 18:17:51 +0300

    Setting standard TidBit Example

--- index.js
+++ index.js
@@ -4,11 +4,24 @@ var express = require('express');
 var app = express();
 const soajsMW = require('soajs.nodejs');
 
+var url = require('url');
+
 app.use(soajsMW({}));
 
-app.get('/hello', function(req, res){
-    console.log(req.soajs)
-    res.send('hello world express');
+app.get('/tidbit/hello', function(req, res){
+	var url_parts = url.parse(req.url, true);
+	var query = url_parts.query;
+	
+	var username = query.username;
+	var lastname = query.lastname;
+	
+	res.send({
+		"message": "Hello, I am an EXPRESS service, you are ["+username+"] and your last name is : ["+lastname+"]"
+	});
+});
+
+app.post('/tidbit/hello', function(req, res){
+	res.send(req.soajs);
 });
 
 app.listen(4381);
