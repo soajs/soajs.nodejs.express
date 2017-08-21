9503e94eae2aafc420bfd0b1fc05861c3bec50bd
antoinehage <me@antoinehage.com>
2017-08-21 15:55:43 -0400

    this a cool demo for paul

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get('/tidbit/hello', function(req, res){
     var lastname = query.lastname;
 
     res.send({
-	    "message": "Hello Rack , I am an EXPRESS service, you are ["+username+"] and your last name is : ["+lastname+"]"
+	    "message": "Hello antoine , I am an EXPRESS service, you are ["+username+"] and your last name is : ["+lastname+"]"
     });
 });
 
