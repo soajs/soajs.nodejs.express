b3fa6717c23ccd306f604a03ebf8553f5a42a8ae
antoinehage <me@antoinehage.com>
2018-01-08 17:33:19 -0500

    cool demo

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get('/tidbit/hello', function(req, res){
     var lastname = query.lastname;
 
     res.send({
-	    "message": "Hello Tony , I am an EXPRESS service, you are ["+username+"] and your last name is : ["+lastname+"]"
+	    "message": "Hello Prem , I am an EXPRESS service, you are ["+username+"] and your last name is : ["+lastname+"]"
     });
 });
 
