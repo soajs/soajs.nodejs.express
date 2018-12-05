c22eb3362aaed8d64748b54bf08cbfc438bcd8a7
antoinehage <me@antoinehage.com>
2018-12-05 15:30:21 -0500

    enhanced version

--- index.js
+++ index.js
@@ -1,40 +1,47 @@
 'use strict';
 
-var express = require('express');
-var app = express();
+let express = require('express');
+let app = express();
 const soajsMW = require('soajs.nodejs');
+const soajsConf = require('./soa.js');
 
-var url = require('url');
+let url = require('url');
 
-app.use(soajsMW({}));
+app.use(soajsMW(soajsConf));
 
-app.get('/tidbit/hello', function (req, res) {
-	var url_parts = url.parse(req.url, true);
-	var query = url_parts.query;
-	
-	var username = query.username;
-	var lastname = query.lastname;
-	
-	//updating something in the code
-	res.send({
-		"message": "Hello, I am an EXPRESS service, you are [" + username + "] and your last name is : [" + lastname + "]"
-	});
+
+app.get('/heartbeat', (req, res) => {
+
+    res.send({"status": 1});
 });
 
-app.post('/tidbit/hello', function (req, res) {
-	
-	var response = req.soajs;
-	
-	req.soajs.awareness.getHost(function (host) {
-		response.controller = host;
-		
-		if (req.soajs.reg) { // if SOAJS_REGISTRY_API is set and everything went well, reg will be defined
-			response.databases = req.soajs.reg.getDatabases();
-		}
-		
-		res.send(response);
-	});
-	
+app.get('/tidbit/hello', (req, res) => {
+    let url_parts = url.parse(req.url, true);
+    let query = url_parts.query;
+
+    let username = query.username;
+    let lastname = query.lastname;
+
+    //updating something in the code
+    res.send({
+        "message": "Hello, I am an EXPRESS service, you are [" + username + "] and your last name is : [" + lastname + "]"
+    });
 });
 
-app.listen(4381);
+app.post('/tidbit/hello', (req, res) => {
+
+    let response = req.soajs;
+
+    req.soajs.awareness.getHost((host) => {
+        response.controller = host;
+
+        if (req.soajs.reg) { // if SOAJS_REGISTRY_API is set and everything went well, reg will be defined
+            response.databases = req.soajs.reg.getDatabases();
+        }
+
+        res.send(response);
+    });
+
+});
+
+app.listen(soajsConf.servicePort);
--- soa.js
+++ soa.js
@@ -1,50 +1,27 @@
 "use strict";
 module.exports = {
-
-	//required fields
-
-		//required by the infra container deployment
-		"type": "service",
-		"prerequisites": {
-			"cpu": " ",
-			"memory": " "
-		},
-
-		//required to support swagger simulator and model injection at runtime
-		"swagger": true,
-
-		//basic service information
-		"serviceName": "express",
-		"serviceGroup": "Custom Services",
-		"serviceVersion": 1,
-		"servicePort": 4381,
-		"requestTimeout": 30,
-		"requestTimeoutRenewal": 5,
-
-		//service configuration
-		"extKeyRequired": true,
-		"oauth": true,
-		"session": false,
-
-	//optional fields
-
-		"urac" : false,
-
-		"urac_Profile" : false,
-		//req.soajs.uracDriver.getProfile() output
-
-		"urac_ACL" : false,
-		//req.soajs.uracDriver.getAcl() output
-		//req.soajs.uracDriver.getAclAllEnv() output
-
-		"provision_ACL" : false,
-		//req.soajs.tenant.application.acl
-		//req.soajs.tenant.application.acl_ell_env
-		//req.soajs.tenant.package.acl
-		//req.soajs.tenant.package.acl_ell_env
-
-
-	//computed fields
-	"errors": {},
-	"schema": {}
+    "type": "service",
+    "prerequisites": {
+        "cpu": " ",
+        "memory": " "
+    },
+    "serviceName": "express",
+    "serviceGroup": "Custom Services",
+    "servicePort": 4381,
+    "swagger": true,
+    "requestTimeout": 30,
+    "requestTimeoutRenewal": 5,
+    "serviceVersion": 1,
+    "extKeyRequired": true,
+    "urac": false,
+    "urac_Profile": false,
+    "urac_ACL": false,
+    "provision_ACL": false,
+    "oauth": true,
+    "maintenance": {
+        "port": {
+            "type": "inherit"
+        },
+        "readiness": "/heartbeat",
+    }
 };
